ad6aa442bbce42febfb89862e856405485379eda
lMartin3 <martinlufr@gmail.com>
2022-10-02 10:43:49 -0300

    build: method to check if someone is subscribed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 import express from "express";
-import mysql from "mysql";
+import mysql from "mysql2";
 
 
 const app = express()
@@ -16,6 +16,16 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
+app.use(function (req, res, next) {
+
+	console.log('Time:', Date.now());
+	if(req.body.token!==process.env.X_API_TOKEN) {
+		res.status(401).send("Unauthorized");
+		return;
+	}
+	next();
+});
+
 
 app.get('/', (req, res) => {
 	res.send(`${process.env.AAA}`);
@@ -25,14 +35,20 @@ app.put('/wa/subscriber', (req, res)=>{
 	console.log(req.body)
 	var parsed = req.body;
 	connection.query("INSERT INTO ignis.WA_SUBSCRIBERS (whatsapp_id, area) VALUES (?,?)", [parsed.whatsapp_id, parsed.area]);
-	res.send("Adding...")
+	res.status(200).send("Added!");
 });
 
 app.delete('/wa/subscriber', (req, res)=>{
 	console.log(req.body)
 	var parsed = req.body;
 	connection.query("DELETE FROM ignis.WA_SUBSCRIBERS WHERE whatsapp_id=?", [parsed.whatsapp_id]);
-	res.send("Adding...")
+	res.status(200).send("Deleted!");
+});
+
+app.get('/wa/subscriber', async (req, res)=>{
+	console.log(req.body);
+	const [rows, fields] = await connection.promise().query("SELECT * FROM ignis.WA_SUBSCRIBERS WHERE whatsapp_id=?", [req.body.whatsapp_id]);
+	res.send(rows);
 });
 
 
@@ -48,3 +64,4 @@ process.on('SIGTERM', () => {
 	console.info('SIGTERM signal received.');
 	connection.end();
 });
+
